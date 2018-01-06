152d0f870c1c28eaede0725e88da9c61ff9f2c3c
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-06 15:27:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,8 +10,8 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do |item, i|
-    "Hello, #{item}! You'll be assigned to room #{i+1}!"
+  attendees.each_with_index.map do |attendee, i|
+    "Hello, #{attendee}! You'll be assigned to room #{i+1}!"
   end
 end
 
