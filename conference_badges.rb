142c501a82b500ffc64b95d63bffb808443375ae
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-06 15:18:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,8 +16,3 @@ def assign_rooms(speakers)
 end
 
 
-def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
-  end
-end
